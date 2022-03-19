e77031d50fbe21a3bc61504fe80c8a79c1561b27
Jason Chen <jason.l.chen@gmail.com>
2022-03-18 17:40:40 -0700

    throwBait and feesh now should work in tandem

--- autoFeeshing.go
+++ autoFeeshing.go
@@ -26,60 +26,65 @@ func feesh() {
   // exit when "x" is pressed
 
   // randomMouseMove
-  isFeeshing := true;
 
   // move mouse to ocean
   // Press "e"
 
   for {
-    rand.Seed(time.Now().UnixNano())
+    if !isBaiting {
+      rand.Seed(time.Now().UnixNano())
+      isFeeshing := true;
+      exPixelCount := 0;
+      pixelColors := [9]string;
 
-    exPixelCount := 0;
-    pixelColors := [9]string;
-
-    // grabs pixel colors in a 3x3 pixel area
-    for i := 0; i <= 3; i++ {
-      for j := 0; j <= 3; j++ {
-        pixelColors[i] = robotgo.GetPixelColor(959 + i, 499 + j);
+      // grabs pixel colors in a 3x3 pixel area
+      for i := 0; i <= 3; i++ {
+        for j := 0; j <= 3; j++ {
+          pixelColors[i] = robotgo.GetPixelColor(959 + i, 499 + j);
+        }
       }
-    }
-    // count pixels that are "yellow"
-    for _, pixelColor := range pixelColors {
-      if strings.Count(pixelColor, "f") <= 4 {
-        exPixelCount++;
+      // count pixels that are "yellow"
+      for _, pixelColor := range pixelColors {
+        if strings.Count(pixelColor, "f") <= 4 {
+          exPixelCount++;
+        }
       }
-    }
 
-    isEx := exPixelCount >= 4;
+      isEx := exPixelCount >= 4;
 
-    // cast line
-    robotgo.KeyPress("e");
-
-    // wait for feesh to bite
-
-
-
-    // reel in feesh
-    if isEx == true {
-      // add random wait time
-      fmt.Println("SUCCESS! " + pixelColor);
-      randomReactionTime := time.Duration(rand.Intn(240 - 180 + 1) + 180);
-      time.Sleep(randomReactionTime * time.Millisecond);
+      // cast line
       robotgo.KeyPress("e");
-      time.Sleep(5 * time.Second);
-      isFeeshing = false;
+
+      // wait for feesh to bite
+
+
+
+      // reel in feesh
+      if isEx {
+        // add random wait time
+        fmt.Println("SUCCESS! " + pixelColor);
+        randomReactionTime := time.Duration(rand.Intn(240 - 180 + 1) + 180);
+        time.Sleep(randomReactionTime * time.Millisecond);
+        robotgo.KeyPress("e");
+        time.Sleep(5 * time.Second);
+        isFeeshing = false;
+      }
     }
+
   }
 }
 
 func throwBait() {
   for {
-    if isFeeshing == true {
+    if isFeeshing true {
       time.Sleep(time.Second * 10);
     } else {
-      time.Sleep(time.Second * 903);
-      mouseX, mouseY := robotgo.GetMousePos();
+      isBaiting = true;
+      // mouseX, mouseY := robotgo.GetMousePos();
       robotgo.KeyPress("d");
+      time.Sleep(time.Second * 2);
+      isBaiting = false;
+      time.Sleep(time.Second * 901);
     }
   }
 }
